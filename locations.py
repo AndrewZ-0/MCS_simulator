d12bcafbff2fb4c278edbb593da8e3a98fa56891
Moses-Bejon <138283384+Moses-Bejon@users.noreply.github.com>
2023-10-24 15:55:24 +0300

    added unload function and a check for if it's loaded

--- locations.py
+++ locations.py
@@ -12,17 +12,28 @@ class location:
 
         self.entities = {"wall":[], "door":[], "furniture":[], "item":[], "student":[], "teacher":[]}
 
+    def loaded(self):
+        return self._loaded
+
     def load(self):
         for dynamics in list(self.entities.values())[2:5]:
             for dynamic in dynamics:
-                if dynamic.getmoved:
+                if dynamic.getmoved():
                     dynamic.randomisexposition(0,self._xwidth) # may be subject to change
                     dynamic.randomiseyposition(0,self._ywidth)
-        GUIsystem.load(self) # Should be a function in GUIsystem, taking the room as an argument
+        self._loaded = True
+        GUIsystem.load(self) # Should be a function in GUIsystem
 
+    def unload(self):
+        for dynamics in list(self.entities.values())[2:5]:
+            for dynamic in dynamics:
+                dynamic.saveLocation()
+        self._loaded = False
+        GUIsystem.unload(self) # this should also be a function in GUIsystem
 
     def enter(self,entity):
         self.entities[entity.getType()].append(entity)
 
     def leave(self,entity):
         self.entities[entity.getType()].remove(entity)
+
