b778b2b08276ea0b71c70f9a9ea53579f4221841
Moses-Bejon <idontwantnumbersinmyname123@gmail.com>
2023-10-25 22:22:36 +0100

    added virtual system and changed virtualx to abs_x (as well as y)

---
+++ Virtual system.py
@@ -0,0 +1,4 @@
+class virtualSystem:
+    def __init__(self):
+        entities = {"wall":[],"door":[],"furniture":[],"item":[],"student":[],"teacher":[]} # global list of entities
+        periodTypes = # list of period types when I can be bothered
--- entities.py
+++ entities.py
@@ -7,21 +7,21 @@ lines = json.load(open("lines.json","r"))
 
 #these are things that move around the rooms, like the player
 class Entity():
-    def __init__(self,name,virtualx,virtualy,location):
+    def __init__(self,name,abs_x,abs_y,location):
         self._name = name
-        self._virtualx = virtualx
-        self._virtualy = virtualy
+        self._abs_x = abs_x
+        self._abs_y = abs_y
         self._location = location # which location, as in which room/field etc
         # GUI logic for instantiating sprite here since all entities have sprites
 
     def getName(self):
         return self._name
 
-    def getVirtualx(self):
-        return self._virtualx
+    def getabs_x(self):
+        return self._abs_x
 
-    def getVirtualy(self):
-        return self._virtualy
+    def getabs_y(self):
+        return self._abs_y
 
     def getLocation(self):
         return self._location
@@ -30,13 +30,13 @@ class Entity():
         return self._type
 
 class Wall(Entity):
-    def __init__(self,name,virtualx,virtualy,location):
-        super().__init__(name,virtualx,virtualy,location)
+    def __init__(self,name,abs_x,abs_y,location):
+        super().__init__(name,abs_x,abs_y,location)
         self._type = "wall"
 
 class Interactable(Entity):
-    def __init__(self,name,virtualx,virtualy,location):
-        super().__init__(name,virtualx,virtualy,location)
+    def __init__(self,name,abs_x,abs_y,location):
+        super().__init__(name,abs_x,abs_y,location)
         self._actions = [] # each action is a tuple, the first element being a string (the description of the action), the second element being a tuple of functions to be performed, the third element a tuple of tuples, each of which are the parameters for the function. i.e ("action description",(print,action),(("action is performed",),(parameter1,parameter2,parameter3)))
         # GUI logic for instantiating nametag here since all interactables have name tags
 
@@ -57,8 +57,8 @@ class Interactable(Entity):
         self._actions = []
 
 class Door(Interactable):
-    def __init__(self,name,virtualx,virtualy,location, open, connects, motion):
-        super().__init__(name, virtualx, virtualy, location)
+    def __init__(self,name,abs_x,abs_y,location, open, connects, motion):
+        super().__init__(name, abs_x, abs_y, location)
         self.__open = open
         self.__connects = connects
         self.__motion = motion
@@ -75,8 +75,8 @@ class Door(Interactable):
         self.__open = False
 
 class GivesBuffs(Interactable):
-    def __init__(self,name,virtualx,virtualy,location,buffs):
-        super().__init__(name,virtualx,virtualy,location)
+    def __init__(self,name,abs_x,abs_y,location,buffs):
+        super().__init__(name,abs_x,abs_y,location)
         self.__buffs = buffs
 
     def getBuffs(self):
@@ -92,13 +92,13 @@ class GivesBuffs(Interactable):
         self.__buffs.remove(buff)
 
 class Furniture(GivesBuffs):
-    def __init__(self,name,virtualx,virtualy,location,buffs):
-        super().__init__(name,virtualx,virtualy,location,buffs)
+    def __init__(self,name,abs_x,abs_y,location,buffs):
+        super().__init__(name,abs_x,abs_y,location,buffs)
 
         self._type = "furniture"
 class Dynamic(GivesBuffs):
-    def __init__(self,name,virtualx,virtualy,location,buffs):
-        super().__init__(name,virtualx,virtualy,location,buffs)
+    def __init__(self,name,abs_x,abs_y,location,buffs):
+        super().__init__(name,abs_x,abs_y,location,buffs)
         self.__moved = False
         # GUI logic for instantiating nametag here
 
@@ -109,36 +109,36 @@ class Dynamic(GivesBuffs):
         self.__moved = False
 
     def randomisexposition(self,min,max):
-        self._virtualx = random.randint(min,max)
+        self._abs_x = random.randint(min,max)
 
     def randomiseyposition(self,min,max):
-        self._virtualy = random.randint(min,max)
+        self._abs_y = random.randint(min,max)
 
     def changeLocation(self,newlocation):
         self._location = newlocation
         self.__moved = True
 
 class Item(Dynamic):
-    def __init__(self,name,virtualx,virtualy,location,buffs):
-        super().__init__(name,virtualx,virtualy,location,buffs)
+    def __init__(self,name,abs_x,abs_y,location,buffs):
+        super().__init__(name,abs_x,abs_y,location,buffs)
 
         self._type = "item"
 
 class Consumable(Item):
-    def __init__(self,name,virtualx,virtualy,location,buffs):
-        super().__init__(name,virtualx,virtualy,location,buffs)
+    def __init__(self,name,abs_x,abs_y,location,buffs):
+        super().__init__(name,abs_x,abs_y,location,buffs)
 
 class Key(Item):
-    def __init__(self,name,virtualx,virtualy,location,buffs):
-        super().__init__(name, virtualx, virtualy, location, buffs)
+    def __init__(self,name,abs_x,abs_y,location,buffs):
+        super().__init__(name, abs_x, abs_y, location, buffs)
 
 class Bag(Item):
-    def __init__(self,name,virtualx,virtualy,location,buffs):
-        super().__init__(name, virtualx, virtualy, location, buffs)
+    def __init__(self,name,abs_x,abs_y,location,buffs):
+        super().__init__(name, abs_x, abs_y, location, buffs)
 
 class Human(Dynamic):
-    def __init__(self,name,virtualx,virtualy,location,buffs,sanity,subjects,mood):
-        super().__init__(name,virtualx,virtualy,location,buffs)
+    def __init__(self,name,abs_x,abs_y,location,buffs,sanity,subjects,mood):
+        super().__init__(name,abs_x,abs_y,location,buffs)
         self._sanity = sanity
         self._subjects = subjects
         self.__defaultMood = mood
@@ -169,8 +169,8 @@ class Human(Dynamic):
             # GUI code for creating speach bubble
 
 class Student(Human):
-    def __init__(self,name,virtualx,virtualy,location,buffs,sanity,subjects,mood,humanities,sciences,otherPeople,popCulture,gaming,sports):
-        super().__init__(name,virtualx,virtualy,location,buffs,sanity,subjects,mood)
+    def __init__(self,name,abs_x,abs_y,location,buffs,sanity,subjects,mood,humanities,sciences,otherPeople,popCulture,gaming,sports):
+        super().__init__(name,abs_x,abs_y,location,buffs,sanity,subjects,mood)
         self.__humanitiesEnjoyment = self.__humanitiesFluency = humanities
         self.__sciencesEnjoyment = self.__sciencesFluency = sciences
         self.__otherPeopleEnjoyment = otherPeople
@@ -219,13 +219,13 @@ class Student(Human):
             student.gainSanity(approval)
 
 class Player(Student):
-    def __init__(self,name,virtualx,virtualy,location,buffs,sanity,subjects,mood,humanities,sciences,otherPeople,popCulture,gaming,sports):
-        super().__init__(name, virtualx, virtualy, location, buffs, sanity, subjects, mood,humanities,sciences,otherPeople,popCulture,gaming,sports)
+    def __init__(self,name,abs_x,abs_y,location,buffs,sanity,subjects,mood,humanities,sciences,otherPeople,popCulture,gaming,sports):
+        super().__init__(name, abs_x, abs_y, location, buffs, sanity, subjects, mood,humanities,sciences,otherPeople,popCulture,gaming,sports)
         self.__stamina = 500
 
 class Teacher(Human):
-    def __init__(self,name,virtualx,virtualy,location,buffs,sanity,subjects,mood,attack):
-        super().__init__(name,virtualx,virtualy,location,buffs,sanity,subjects,mood)
+    def __init__(self,name,abs_x,abs_y,location,buffs,sanity,subjects,mood,attack):
+        super().__init__(name,abs_x,abs_y,location,buffs,sanity,subjects,mood)
         self.__attack = attack
 
         self._type = "teacher"
