bf141967a8309c9925944df8dde46c748f0abb61
AndrewZ-0 <107640838+AndrewZ-0@users.noreply.github.com>
2023-10-25 16:48:20 +0200

    Add files via upload

--- GuiElements.py
+++ GuiElements.py
@@ -22,7 +22,7 @@ class dataBar:
 
         self.data_font = pygame.font.SysFont("Calibri", 15, bold = True)
 
-        self.update(value)
+        self.update(0)
 
     def get_barWidth(self):
         return int((self.value / self.maxvalue) * (self.width - 2))
@@ -43,6 +43,9 @@ class dataBar:
         green = self.update_colour(self.colour[1], greenChange)
         blue = self.update_colour(self.colour[2], blueChange)
 
+        self.set_colours(red, green, blue)
+    
+    def set_colours(self, red, green, blue):
         self.colour = (red, green, blue)
     
     def set_warningBorder(self, warningFlag):
@@ -53,8 +56,8 @@ class dataBar:
             self.outlineWidth = 1
             self.outlineColour = (100, 100, 100)
 
-    def update(self, newValue):
-        self.value = newValue
+    def update(self, valueChange):
+        self.value += valueChange
 
         dataText_text = self.get_dataText_text()
 
@@ -66,7 +69,7 @@ class dataBar:
     def draw(self):
         pygame.draw.rect(self.screen, (150, 150, 150), self.barContainerBg)
         pygame.draw.rect(self.screen, self.outlineColour, self.barContainerOutline, self.outlineWidth)
-        pygame.draw.rect(self.screen, self.colour, self.bar)
+        pygame.draw.rect(self.screen, (int(self.colour[0]), int(self.colour[1]), int(self.colour[2])), self.bar)
 
         self.screen.blit(self.dataText, self.text_rect)
 
@@ -76,26 +79,44 @@ class SanityBar(dataBar):
         sanity = 100
         self.regenTicker = 0
 
-        super().__init__(screen, sanity, sanity, (0, 176, 24), x, y)
+        #(0, 176, 24)
+        super().__init__(screen, sanity, sanity, (0, 200, 0), x, y)
 
-    def update(self, newValue):
+    def update(self, valueChange):
+        newValue = round(self.value + valueChange, 1)
+
+        if valueChange < 0:
+            direction = 1
+        elif valueChange > 0:
+            direction = -1
+
         if newValue <= 0: #0%
             self.value = 0
             print("gameEnd")
-        elif int(newValue) <= 10: #5%
-            #self.update_colour(153, 0, 0)
-            self.update_colours(-2, -1, 0)
-        elif self.maxvalue / newValue >= 8: #12.5%
-            self.update_colours(0, -10, -6)
-            #self.update_colour(255, 17, 0)
-        elif self.maxvalue / newValue >= 4: #25%
-            self.update_colours(10, -1, -1)
-            #self.update_colour(255, 127, 66)
-        #else:
-            #self.update_colour(0, 176, 24)
+        elif newValue <= 5: #5%
+            #0-5
+            self.set_colours(255, 0, 0)
+            #self.update_colours(-2 * direction, -1 * direction, 0)
+        elif newValue <= 10: #valueRatio >= 8: #12.5%
+            #5-10
+            #self.update_colours(0, -10 * direction, -6 * direction)
+            j = ((10 - 5) / 0.2)
+            self.update_colours(0, direction * ((0 - 127) / j), direction * ((0 - 66) / j))
+            #(255, 17, 0)
+        elif newValue <= 25: #self.maxvalue / newValue >= 4: #25%
+            #10-25
+            #self.update_colours(10 * direction, -1 * direction, -1 * direction)
+            j = ((25 - 10) / 0.2)
+            self.update_colours(direction * (255 / j), direction * ((127 - 200) / j), direction * ((66 - 24) / j))
+            #(255, 127, 66)
+        else:
+            #25-100
+            #(0, 176, 24)
+            self.set_colours(0, 200, 0)
+        print(self.colour, newValue)
         
         self.regenTicker = 0
-        super().update(newValue)
+        super().update(valueChange)
 
         if int(self.value) == 0:
             self.set_warningBorder(True)
@@ -109,8 +130,8 @@ class StaminaBar(dataBar):
 
         super().__init__(screen, stamina, stamina, (5, 213, 250), x, y)
     
-    def update(self, newValue):
-        super().update(newValue)
+    def update(self, valueChange):
+        super().update(valueChange)
 
         if self.fatigued and self.value > 10:
             self.fatigued = False
