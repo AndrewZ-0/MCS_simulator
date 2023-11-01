4254a8c5a9f783a8c88f32f0c8e29bd9f0b7007e
Moses-Bejon <idontwantnumbersinmyname123@gmail.com>
2023-11-01 14:17:25 +0000

    slight error fixed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
 from VirtualSystem import virtualSystem
 
-pause = False # this is a global variable used to pause all the entities
 loadedLocations = [] # may be subject to change, contains locations that are currently loaded
 system = virtualSystem()
