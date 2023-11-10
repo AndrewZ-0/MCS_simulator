3a7f69669153b6d1b63f9b3e225d0d17835817cf
Moses-Bejon <idontwantnumbersinmyname123@gmail.com>
2023-11-10 11:30:20 +0000

    mcs_map.json now loads into python

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from VirtualSystem import virtualSystem
 from locations import LocationsHandler
+import json
 
-loadedLocations = LocationsHandler.getLoadedLocations()
+locationsHandler = LocationsHandler
 system = virtualSystem()
+
+loadedLocations = locationsHandler.getLoadedLocations()
+
+# load CS building (potentially only building in early game
+with json.load(open("mcs_map.json"))["locations"]["collin sanders"] as csBuilding:
+    for location in csBuilding.items():
+        system.instantiateLocation(location[0],location[1][2],location[1][3],location[1][4],location[1][5],location[1][6],location[1][7])
+
